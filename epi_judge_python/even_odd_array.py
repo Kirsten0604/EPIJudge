828a030f6f49cf84f9e021c5abfa07f554c541dc
Kirsten Takanishi <khtakani@hawaii.edu>
2019-05-16 16:35:06 -1000

    even odd

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -7,9 +7,14 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def even_odd(A):
-    # TODO - you fill in here.
-    return
-
+    i = 0
+    j = len(A) - 1
+    while i < j:
+        if A[i] % 2 == 0:
+            i += 1
+        else:
+            A[i], A[j] = A[j], A[i]
+            j -= 1
 
 @enable_executor_hook
 def even_odd_wrapper(executor, A):
