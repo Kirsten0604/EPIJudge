4f380c829f87dd96f02f2844cd691cace8ace6fc
Kirsten Takanishi <khtakani@hawaii.edu>
2019-05-27 01:34:43 -1000

    offline

--- epi_judge_python/offline_sampling.py
+++ epi_judge_python/offline_sampling.py
@@ -1,4 +1,5 @@
 import functools
+import random
 
 from test_framework import generic_test
 from test_framework.random_sequence_checker import (
@@ -8,9 +9,9 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def random_sampling(k, A):
-    # TODO - you fill in here.
-    return
-
+    for i in range(k):
+        r = random.randint(i, len(A) - 1)
+        A[i], A[r] = A[r], A[i]
 
 @enable_executor_hook
 def random_sampling_wrapper(executor, k, A):
