557e7e1ad9872e6f4586c50251bc101e3a16627b
Kirsten Takanishi <khtakani@hawaii.edu>
2019-06-20 20:10:49 -1000

    replace and remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -5,8 +5,27 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size, s):
+    a_count = 0
+    write_index = 0
+    for i in range(size):
+        if s[i] == 'a':
+            a_count += 1
+        if s[i] != 'b':
+            s[write_index] = s[i]
+            write_index += 1
 
-    return 0
+    curr_index = write_index - 1
+    write_index += a_count - 1
+    final_size = write_index + 1
+    while curr_index >= 0:
+        if s[curr_index] == 'a':
+            s[write_index - 1:write_index + 1] = 'dd'
+            write_index -= 2
+        else:
+            s[write_index] = s[curr_index]
+            write_index -= 1
+        curr_index -= 1
+    return final_size
 
 
 @enable_executor_hook
